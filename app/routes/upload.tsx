f5c3b1022125d9ee3fc6f1477aa209a96a00e450
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-08-02 19:29:08 +0530

    upload form creation

---
+++ app/routes/upload.tsx
@@ -0,0 +1,43 @@
+import {type FormEvent, useState} from "react";
+import Navbar from "~/components/Navbar";
+
+const upload = () => {
+    const [isProcessing, setIsProcessing] = useState(false);
+    const [statusText, setStatusText] = useState(' ');
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {}
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading">
+            <h1>Smart feedback for your dream job</h1>
+            {isProcessing ? (
+                <>
+                <h2>{statusText}</h2>
+                <img src="/images/resume-scan.gif" className="w-full" />
+                </>
+            ) : (
+                <h2>
+                    Drop your resume for an ATS score and improvment tips
+                </h2>
+            )}
+            {!isProcessing && (
+                <form id="upload-form" onSubmit={handleSubmit} className="flex flex-col gap-4">
+                    <div className="form-div">
+                        <label htmlFor="company-name">
+                            Company Name
+                        </label>
+                        <input type="text" name="company-name" placeholder="Company Name" id= "company-name" />
+                    </div>
+                </form>
+            )}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default upload;
