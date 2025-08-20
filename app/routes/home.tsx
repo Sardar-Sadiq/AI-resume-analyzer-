5555d240bc460de660e84db6e2ec6bdbab4e728e
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-08-20 17:53:33 +0530

    home, navbar and resumecard compo updated

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate, type NavigateFunction } from 'react-router';
 import { usePuterStore } from '~/lib/puter'
 
@@ -14,23 +14,20 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const {  auth, fs } = usePuterStore();
+  const {  auth, kv } = usePuterStore();
     const navigate: NavigateFunction = useNavigate();
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loadingResumes, setLoadingResumes] = useState(false);
     
-
+ 
     useEffect(() => {
         if(!auth.isAuthenticated) navigate('/auth?next=/');
     }, [auth.isAuthenticated])
 
-//2:17:00 start from here another day for another commit ok one last time
-    useEffect(() => {
-        const loadResume = async() => {
-            const blob = await fs.read(resume.imagePath);
-            if(!blob) return;
-            let url = URL.createObjectURL(blob);
+    useEff
 
-        }
-    }, []);
+//2:17:00 start from here another day for another commit ok one last time
+    
 
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
